37dbf7ed5543b36d6a5726cb087c98ad9619b895
lenlis <sit.a.v@mail.ru>
2024-04-29 14:00:51 +0500

    Страница выбора

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './nullstyle.scss'
 import { Route, Routes } from 'react-router-dom'
 import Main from './Page/Main'
 import CharacterSelection from './Page/characterSelection'
+import ChoiceSection from './Page/choiceSection'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
           <Route path="/Man-in-an-era-of-upheaval" element={<Main />} />
           <Route path="/Man-in-an-era-of-upheaval/character-selection" element={<CharacterSelection />} />
+          <Route path="/Man-in-an-era-of-upheaval/game" element={<ChoiceSection />} />
       </Routes>
     </div>
   )
---
+++ src/Page/choiceSection.tsx
@@ -0,0 +1,13 @@
+import Header from '../Components/Header/Header';
+import GameChoise from '../Components/GameChoice/GameChoice';
+
+const ChoiceSection = () => {
+    return (
+        <>
+            <Header />
+            <GameChoise />
+        </>
+    );
+};
+
+export default ChoiceSection;
